3e3d6a8f887dec225744e5fb2c02543d12311100
joseavalos1998 <80939325+joseavalos1998@users.noreply.github.com>
2021-03-18 23:32:51 -0600

    Add files via upload

---
+++ Itesco/ex_1_1.py
@@ -0,0 +1,10 @@
+print('Pepito')
+print('Cumpleaños: 5 de octubre')
+edad = 22
+print('Tengo', edad, 'años')
+cantante = 'Avicii'
+comida = 'tacos'
+ciudad = 'Coatzacoalcos'
+print('Me gusta la música de', cantante)
+print('Me gusta cenar', comida)
+print('Vivo en', ciudad)
---
+++ Itesco/ex_1_2_b.py
@@ -0,0 +1,5 @@
+print('Suma de dos reales')
+a = float( input('Entre el dato 1:65 ') )
+b = float( input('Entre el dato 2:54 ') )
+suma = a + b
+print('La suma vale:' , suma)
